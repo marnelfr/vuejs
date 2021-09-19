78f0d2e33cdf5306a54c0b1083b4a9621d5d6be7
Marnel Gnacadja <marnel.gnacadja@paydunya.com>
2021-09-19 15:51:01 +0100

    Nested component

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
     <h1>Event listing page</h1>
+    <EventCard />
     <router-link :to="{ name: 'event-show', params: { id: 1 }}">Event show #1</router-link>
   </div>
 </template>
+<script>
+import EventCard from '@/components/EventCard.vue';
+export default {
+  components: {
+    EventCard,
+  },
+}
+</script>
