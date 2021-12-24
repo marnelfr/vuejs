83c0d298edd2da471b5ce3293af2d23219540f74
Marnel Gnacadja <marnel.gnacadja@paydunya.com>
2021-12-24 12:39:37 +0100

    Start working on turing vue router course

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -7,35 +7,65 @@
     <div v-else>
       <p>Loading...</p>
     </div>
-    
+    <div class="pagination">
+      <router-link
+        id="prev-link"
+        v-if="page > 1"
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        rel="prev"
+      >
+        &#60; Previous
+      </router-link>
+      <router-link
+        id="next-link"
+        v-if="hasNextPage"
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        rel="next"
+      >
+        Next &#62;
+      </router-link>
+    </div>
   </div>
 </template>
-
 <script>
 // @ is an alias to /src
-import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService.js'
+import EventCard from "@/components/EventCard.vue";
+import EventService from "@/services/EventService.js";
+import { watchEffect } from "vue";
 
 export default {
-  name: 'EventList',
+  name: "EventList",
+  props: ["page"],
   components: {
     EventCard,
   },
   data() {
     return {
       events: null,
-    }
+      totalEvent: 0,
+    };
   },
   created() {
-    EventService.getEventList()
-      .then((response) => {
-        this.events = response.data
-      })
-      .catch((error) => {
-        console.log('error', error)
-      })
+    watchEffect(() => {
+      this.events = null;
+      EventService.getEventList(this.page)
+        .then((response) => {
+          this.events = response.data;
+          this.totalEvent = response.headers["x-total-count"];
+          console.log(this.totalEvent);
+        })
+        .catch((error) => {
+          console.log("error", error);
+        });
+    });
   },
-}
+  computed: {
+    hasNextPage() {
+      const totalPage = Math.ceil(this.totalEvent / 2);
+      return this.page < totalPage;
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -43,5 +73,18 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  text-align: center;
+}
+.pagination a {
+  flex: 1;
+}
+#prev-link {
+  float: left;
+}
+#next-link {
+  float: right;
 }
 </style>
