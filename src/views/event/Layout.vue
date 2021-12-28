5ae3ccc06e6300a50d6458fb7521a397c0c5942e
Marnel Gnacadja <marnel.gnacadja@paydunya.com>
2021-12-28 15:10:50 +0100

    Nested routes can be set without their params

--- src/views/event/Layout.vue
+++ src/views/event/Layout.vue
@@ -3,15 +3,9 @@
     <h1>{{ event.title }}</h1>
 
     <div class="nav">
-      <router-link :to="{ name: 'EventDetails', params: { id } }"
-        >Details</router-link
-      >
-      <router-link :to="{ name: 'EventRegister', params: { id } }"
-        >Register</router-link
-      >
-      <router-link :to="{ name: 'EventEdit', params: { id } }"
-        >Edit</router-link
-      >
+      <router-link :to="{ name: 'EventDetails' }">Details</router-link>
+      <router-link :to="{ name: 'EventRegister' }">Register</router-link>
+      <router-link :to="{ name: 'EventEdit' }">Edit</router-link>
     </div>
 
     <router-view :event="event" />
